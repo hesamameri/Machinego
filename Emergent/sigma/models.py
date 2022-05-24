3066cbf2682565c9411b11b46b61e7fc881f51ce
hesamameri <hesam7991ameri@gmail.com>
2022-05-24 18:40:28 +0430

    django-models-arguments_added-03/3/1401

--- Emergent/sigma/models.py
+++ Emergent/sigma/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Argument(models.Model):
+    
+    title = models.CharField(max_length=100)
+    aim = models.CharField(max_length=50)
+    content = models.TextField()
+    author = models.ForeignKey(to='Person', on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self): 
+        return self.title
+
+
+
+class Meta:
+    verbose_name = "Argument"
+    verbose_name_plural = "Arguments"
